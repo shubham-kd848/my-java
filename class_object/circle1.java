284a34051e4f039a24fb4d49740ed2ed7dddf6f1
shubham-kd848 <shubham.kedar2006@gmail.com>
2025-12-26 09:49:47 -0800

    normal

--- class_object/circle1.java
+++ class_object/circle1.java
@@ -1,22 +1,27 @@
  class circle
 {
   public  double radius;
-   public double area(double r)
+   public double area()
    {
-    return 3.14*r*r;
+    return 3.14*radius*radius;
 
    }
-   public double perimeter(double r)
+   public double perimeter()
    {
-    return 2*3.14*r;
+    return 2*3.14*radius;
    }
 }
 public class circle1
 {
     public static void main(String[] args)
     {
+        circle c2=new circle();
         circle c1=new circle();
-        System.out.println("area is"+c1.area(4));
-        System.out.println("perimeter is"+c1.perimeter(4));
+        c1.radius=12;
+        c2.radius=34;
+        System.out.println("area is"+ c1.area());
+        System.out.println("perimeter is" +c1.perimeter());
+        System.out.println("area is" +c2.area());
+        System.out.println("perimeter is" +c2.perimeter());
     }
 }
